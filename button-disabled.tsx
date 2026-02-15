ab367a7a1315baf6bd90f522c7f96b62ac787436
Pedro Henrique Sanson <156597965+sansonpedro@users.noreply.github.com>
2026-02-15 09:53:53 -0300

    syntatic sugar examples

--- button-disabled.tsx
+++ button-disabled.tsx
@@ -14,12 +14,6 @@ export default Button
 
 
 // boas práticas de nomeação de variáveis
-// 1. nomeação clara
-// 2. nomeação descritiva
-// 3. nomeação consistente
-// 4. nomeação legível
-// 5. nomeação manutenível
-// 6. nomeação escalável
-// 7. nomeação performática
-// 8. nomeação segura
-// 9. pensar na causa dela, não no efeito, para nomear
+// 1. pensar na causa dela, não no efeito, para nomear
+// 2. nomeação clara
+// 3. nomeação descritiva
---
+++ syntatic-sugars.tsx
@@ -0,0 +1,8 @@
+const numberInString = "123";
+
+// const number = +numberInString;
+// const number = parseInt(numberInString);
+
+const number = Number(numberInString);
+const isNumberNotNull = Boolean(number);
+const numberAsString = String(number);
